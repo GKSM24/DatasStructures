beb13d4704552f425d9b2df3ded253964d7465f6
GKSM24 <kkausshawlsaim24@gmail.com>
2019-11-11 23:30:27 +0530

    module 2 assignments

---
+++ src/module2/assignment4/Solution.java
@@ -0,0 +1,77 @@
+package module2.assignment4;
+
+class Solution{
+    static class CircularLinkedList{
+        Node head, tail;
+        class Node{
+            int data;
+            Node next;
+        }
+        void insert(int data){
+            Node obj = new Node();
+            obj.data = data;
+            if(head == null){
+                head = obj;
+                tail = obj;
+            }
+            else{
+                tail.next = obj;
+                tail = obj;
+            }
+            tail.next = head;
+        }
+        int length(){
+            int count = 0;
+            if(head != null){
+                Node temp = head;
+                count = 1;
+                while(temp.next != head){
+                    temp = temp.next;
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+    public static String Josephus(int a, int b){
+        CircularLinkedList list = new CircularLinkedList();
+        String printString = "";
+        for(int i = 0; i < a; i++)
+            list.insert(i);
+        CircularLinkedList.Node current = list.head;
+        while(list.length() > 1){
+            int position = b;
+            while(position > 1){
+                current = current.next;
+                position--;
+            }
+            if(current == list.head){
+                CircularLinkedList.Node temp = list.head;
+                printString += current.data+" ";
+                list.head = list.head.next;
+                temp.next = null;
+                current = list.head;
+                list.tail.next = list.head;
+            }
+            else{
+                CircularLinkedList.Node temp = list.head;
+                while(temp.next != current)
+                    temp = temp.next;
+                if(current == list.tail){
+                    printString += current.data+" ";
+                    temp.next = list.head;
+                    list.tail = temp;
+                    current = list.head;
+                }
+                else{
+                    printString += current.data+" ";
+                    temp.next = current.next;
+                    current.next = null;
+                    current = temp.next;
+                }
+            }
+        }
+        printString += current.data;
+        return printString;
+    }
+}
